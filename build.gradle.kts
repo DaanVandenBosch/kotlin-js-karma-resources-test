75f58fa102cd552877092242e3d55a3442538a3e
Daan Vanden Bosch <daan.v.d.bosch@gmail.com>
2020-11-13 19:09:42 +0100

    Fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,3 +30,17 @@ kotlin {
         }
     }
 }
+
+val generateKarmaConfig = tasks.register("generateKarmaConfig") {
+    val outputFile = file("karma.config.d/karma.config.generated.js")
+
+    outputs.file(outputFile)
+
+    outputFile.printWriter().use { writer ->
+        writer.println("var PROJECT_PATH = '${projectDir.absolutePath.replace("\\", "\\\\")}';")
+    }
+}
+
+tasks.getByName("browserTest") {
+    dependsOn(generateKarmaConfig)
+}
